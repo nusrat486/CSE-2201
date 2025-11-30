bf7a5a5dc4bf9bdfe781346b0228279f2bd3bdf8
nusrat486 <nusrat.cse2022cstu@gmail.com>
2025-11-30 09:40:14 +0600

    Add files via upload

--- bfs.cpp
+++ bfs.cpp
@@ -1,1 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"Enter number of nodes and edges"<<endl;
+    int n,m;
+    cin>>n >> m;
+    int adj[n+1][n+1]={0};
+    cout<<"enter edges"<<endl;
+    for(int i=0;i<m;i++)
+    {
+        int u,v;
+        cin>> u >> v;
+        adj[u][v]=1;
+        adj[v][u]=1;
+    }
+    int start;
+    cout<<"Enetr starting node"<<endl;
+    cin>>start;
+    bool visited[n+1]={0};
+    queue<int>q;
+    visited[start]=true;
+    q.push[start];
+    cout<<"BFS traversal"<<endl;
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+        cout<<node<<" ";
+
+        for(int i=1;i<=n;i++)
+        {
+            if (adj[node][i]&&!visited[i]) {
+                visited[i]=true;
+                q.push(i);
+            }
+        }
+    }
+    return 0;
+        }
+
+
 
